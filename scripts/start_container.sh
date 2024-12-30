13669c713cc3e6d28f1fbb275a3d3cbedf063cb8
Purushotham Kandheri <87255292+purushotham5@users.noreply.github.com>
2024-12-30 22:49:53 +0530

    vr

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 #pull the docker image from the docker hub
-docker pull purushotham5/sample-python-flask-app:latest
+docker pull purushotham5/sample-python-flask-app
 
 
 #to run the docker image as a contianer
-docker run -d -p 5000:5000 purushotham5/sample-python-flask-app:latest
+docker run -d -p 5000:5000 purushotham5/sample-python-flask-app 
